0a924c99bcea1233c095f0dbd1e6e2f161ddd6be
Patrick Karjala <pkarjala@hawaii.edu>
2015-03-12 21:21:41 -1000

    Finished E42 WOD first attempt.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.formdata.ContactFormData;
 import views.html.Index;
 import views.html.NewContact;
 
@@ -20,11 +22,25 @@ public class Application extends Controller {
 
   /**
    * Returns newcontact, a simple example of a second page to illustrate navigation.
+   * Handles form data as well.
    * @return The NewContact.
    */
   public static Result newcontact() {
-    return ok(NewContact.render("Welcome to NewContact."));
+    Form<ContactFormData> formData = Form.form(ContactFormData.class);
+    return ok(NewContact.render(formData));
+  }
 
+  /**
+   * Returns the results of the post data from NewContact submission.
+   *
+   * @return Post data from the form.
+   */
+  public static Result postContact() {
+    System.out.println("In post Contact");
+    Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
+    ContactFormData data = formData.get();
+    System.out.format("%s, %s, %s%n", data.firstName, data.lastName, data.telephone);
+    return ok(NewContact.render(formData));
   }
 
 }
---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,14 @@
+package views.formdata;
+
+/**
+ * Created by Patrick A. Karjala on 3/12/15.
+ *
+ */
+public class ContactFormData {
+
+  public String firstName = "";
+
+  public String lastName = "";
+
+  public String telephone = "";
+}
