b554afab7abede38fa9322f498cce0bd3fc3a16e
Patrick Karjala <pkarjala@hawaii.edu>
2015-03-14 13:20:11 -1000

    Finished WOD.

---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,17 @@
+package views.formdata;
+
+/**
+ * ContactFormData allows for the storage of Form Data that is input by the user.
+ */
+public class ContactFormData {
+
+  /** Input data first name field. */
+  public String firstName = "";
+
+  /** Input data last name field. */
+  public String lastName = "";
+
+  /** Input data telephone field. */
+  public String telephone = "";
+
+}
